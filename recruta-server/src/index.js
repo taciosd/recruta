4eabd6e56b972c55e7778ffe1654f29a6b965cb0
Tacio S. Diogo <taciosd@gmail.com>
2020-03-07 18:48:01 -0300

    Fix heroku configuration

--- recruta-server/src/index.js
+++ recruta-server/src/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const passport = require('passport');
+const path = require('path');
+//const passport = require('passport');
 
 mongoose.connect('mongodb+srv://taciosd:cZD9260IbVlmvJjE@cluster0-q33qb.mongodb.net/test?retryWrites=true&w=majority', 
     {
@@ -19,7 +20,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/../../recruta-web/build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../../recruta-web/build/index.html'));
+});
 app.use(routes);
-app.use(passport.initialize());
+//app.use(passport.initialize());
 
-app.listen(3333);
+const PORT = process.env.PORT || 3333
+app.listen(PORT);
